9c5714e320ade8f4d2504158dc867a416d434c26
ADAM SMITH <>
2019-05-18 22:53:13 +0100

    Made a start on slider title

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,70 +1,94 @@
 <template>
-
+  <div class="container main-carousel">
     <div class="container-content">
-        <div class="row">
-          <div class="col s12 push-l1 l10" style="outline: 2px solid orange;">
-            <div class="main-carousel" v-on:wheel="mouseScroll">
-              <div v-for="item in items" v-bind:key="item.id" class="main-carousel__cell">
-                <div class="main-carousel__spacer"></div>
-                <img :src="getImgURL(item, item['slider-image-src'])" alt="" class="main-carousel__image">
-                <!-- {{item.title}} -->
-                <!-- <router-link :to="getprojectLink(item)">View Project</router-link> -->
-              </div>
+        <h2 class="main-carousel__title" js-slider="title"></h2>  
+        <div class="main-carousel__container">
+          <div class="main-carousel__carousel" v-on:wheel="mouseScroll">
+            <div v-for="item in items" v-bind:key="item.id" class="main-carousel__cell" :data-project="item.title">
+              <div class="main-carousel__spacer"></div>
+              <img
+                :src="getProjectURL(item, item['slider-image-src'])"
+                alt
+                class="main-carousel__image"
+              >
+              <!-- <router-link :to="getprojectLink(item)">View Project</router-link> -->
             </div>
           </div>
         </div>
     </div>
-
+  </div>
 </template>
 
 <script>
-  import projects from './json/projects.json'
-  import debounce from 'debounce'
+import projects from "./json/projects.json";
+import debounce from "debounce";
 
-  export default {
-    name: "home",
-    data() {
-      return {
-        items: projects,
-        slider: {},
-      };
+export default {
+  name: "home",
+  data() {
+    return {
+      items: projects,
+      slider: {},
+      title: {}
+    };
+  },
+  mounted() {
+    this.buildSlider();
+    this.title = document.querySelector('[js-slider="title"]');
+  },
+  methods: {
+    getProjectURL(item, img) {
+      return require(`../images/projects/${item.path}/${img}`);
     },
-    mounted() {
-      this.buildSlider();
-      console.log(debounce);
+    getprojectLink(item) {
+      return `/project/${item.path}`;
     },
-    methods: {
-      getImgURL(item, img) {
-        return require(`../images/projects/${item.path}/${img}`);
-      },
-      getprojectLink(item) {
-        return `/project/${item.path}`;
-      },
-      buildSlider() {
-        this.slider = new this.$tns({
-          container: '.main-carousel',
-          items: 1,
-          slideBy: 'page',
-          autoplay: false,
-          axis: 'vertical',
-          gutter: 0,
-          center: true,
-          controls: false,
-          arrowKeys: true,
-          speed: 1000,
-          autoplayHoverPause: true,
-          touch: true,
-          mouseDrag: false,
-          nav: false
-        });
-      },
-      mouseScroll: debounce(function(e){
-        if (e.deltaY < 0) {
-          this.slider.goTo('prev');
-        } else {
-          this.slider.goTo('next');
+    buildSlider() {
+      const self = this;
+      this.slider = new this.$tns({
+        container: ".main-carousel__carousel",
+        items: 1,
+        slideBy: "page",
+        autoplay: false,
+        axis: "vertical",
+        gutter: 0,
+        center: true,
+        controls: false,
+        arrowKeys: true,
+        speed: 1500,
+        autoplayHoverPause: true,
+        touch: true,
+        mouseDrag: false,
+        nav: false,
+        onInit: function() {
+          self.sliderEvents();
         }
-      } ,300)
-    }
-  };
+      });
+    },
+    sliderEvents() {
+      let self = this;
+      this.slider.events.on("transitionStart", function() {
+        console.log("transitionStart");
+        //self.clearTitle();
+      });
+      this.slider.events.on("transitionEnd", function() {
+        console.log("transitionEnd");
+        //self.populateTitle();
+      });
+    },
+    clearTitle() {
+      //console.log('clearTitle');
+    },
+    populateTitle() {
+
+    },
+    mouseScroll: debounce(function(e) {
+      if (e.deltaY < 0) {
+        this.slider.goTo("prev");
+      } else {
+        this.slider.goTo("next");
+      }
+    }, 300)
+  }
+};
 </script>
