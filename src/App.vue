e5e46641d5c39eb6f53ce92570d599b56d528708
ADAM SMITH <>
2019-05-18 16:34:50 +0100

    Made a start on tiny slider

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <header>
-      <router-link to="/">Home</router-link>
-      <nav>
-        <router-link to="/about">About</router-link>
-        <router-link to="/contact">Contact</router-link>
-      </nav>
-    </header>
+    
+    <div class="container">
+      <div class="row">
+        <header>
+          <img src="./assets/logo.png" class="logo">
+          <router-link to="/">Home</router-link>
+          <nav>
+            <router-link to="/about">About</router-link>
+            <router-link to="/contact">Contact</router-link>
+          </nav>
+        </header>
+      </div>
+    </div>
 
     <!-- <transition
         name="fade"
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="page">
-        <div class="container">
 
-          <div class="test"></div>
-
+    <div class="container-content">
+        <div class="row">
+          <div class="col s12 push-l1 l10" style="outline: 2px solid orange;">
             <div class="main-carousel">
-              <div v-for="item in items" v-bind:key="item.id" class="carousel-cell">
-                <img :src="getImgURL(item, item['slider-image-src'])" alt="">
+              <div v-for="item in items" v-bind:key="item.id" class="main-carousel__cell">
+                <div class="main-carousel__spacer"></div>
+                <img :src="getImgURL(item, item['slider-image-src'])" alt="" class="main-carousel__image">
                 <!-- {{item.title}} -->
                 <!-- <router-link :to="getprojectLink(item)">View Project</router-link> -->
               </div>
             </div>
-
+          </div>
         </div>
     </div>
+
 </template>
 
 <script>
@@ -27,44 +28,36 @@
       };
     },
     mounted() {
-      this.isImageLoaded();
-      //https://github.com/ganlanyuan/tiny-slider
-      //https://vanillalist.top/
+      this.buildSlider();
     },
     methods: {
       getImgURL(item, img) {
-        console.log(`../images/projects/${item.path}/${img}`);
         return require(`../images/projects/${item.path}/${img}`);
       },
       getprojectLink(item) {
         return `/project/${item.path}`;
       },
       buildSlider() {
-        console.log(this.$tns);
-        // const slider = new this.$flickity( '.main-carousel', {
-        //   prevNextButtons: false,
-        //   wrapAround: false,
-        //   selectedAttraction: 0.01,
-        //   friction: 0.15,
-        //   contain: false,
-        //   pageDots: false,
-        //   on: {
-        //     ready: function() {
-        //       console.log('Flickity ready');
-        //     }
-        //   }
-        // });
-      },
-      isImageLoaded() {
-        let image = document.querySelectorAll('.carousel-cell img')[0];
-        let callback = this.isImageLoaded;
-        if (image.offsetHeight < 1) { 
-          setTimeout(function() {
-            callback();
-          }, 100); 
-        } else {
-          this.buildSlider();
-        }
+
+        const slider = new this.$tns({
+          container: '.main-carousel',
+          items: 1,
+          slideBy: 'page',
+          autoplay: false,
+          axis: 'vertical',
+          gutter: 30,
+          center: true,
+          controls: false,
+          arrowKeys: true,
+          speed: 1000,
+          autoplayHoverPause: true,
+          touch: true,
+          mouseDrag: true,
+          nav: false,
+          onInit: () => {
+            console.log('inited');
+          }
+        });
       }
     }
   };
